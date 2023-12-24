d8aab22c5c4511e66982fc89d025a0b51c914155
Miunn <remcaulier@orange.fr>
2023-12-24 18:57:58 +0100

    Export bottles list

---
+++ src/components/BottlesList.tsx
@@ -0,0 +1,60 @@
+import {useIsFocused, useTheme} from "@react-navigation/native";
+import {useCallback, useEffect, useState} from "react";
+import {BottleType} from "../models/BottleType.tsx";
+import {getBottles, getDBConnection, initDB} from "../services/db-interface.ts";
+import {ScrollView, Text} from "react-native";
+import {List} from "react-native-paper";
+
+const BottlesList = () => {
+    const isFocused = useIsFocused();
+    const {colors} = useTheme();
+
+    const [bottles, setBottles] = useState<BottleType[]>([]);
+    const loadBottlesCallback = useCallback(async () => {
+        try {
+            const db = await getDBConnection();
+            await initDB(db);
+            const storedBottles = await getBottles(db);
+            setBottles(storedBottles);
+        } catch (err) {
+            console.error(err);
+        }
+    }, []);
+
+    useEffect(() => {
+        loadBottlesCallback();
+    }, [loadBottlesCallback, isFocused, bottles]);
+
+    return (
+        <ScrollView>
+            <Text>Toutes les bouteilles</Text>
+            {bottles.map((bottle) => {
+                let colorString;
+                switch (bottle.color) {
+                    case 'red':
+                        colorString = "Vin Rouge";
+                        break;
+                    case 'white':
+                        colorString = "Vin Rouge";
+                        break;
+                    case 'pink':
+                        colorString = "Vin Rouge";
+                        break;
+                    default:
+                        colorString = `Vin ${bottle.color}`;
+                }
+
+                return (
+                    <List.Item
+                        key={bottle.id}
+                        title={bottle.name}
+                        description={`${bottle.vintageYear} - ${colorString}`}
+                        left={props => <List.Icon {...props} icon={"bottle-wine"} /> }
+                    />
+                )
+            })}
+        </ScrollView>
+    )
+}
+
+export default BottlesList;
