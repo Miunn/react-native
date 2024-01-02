646f5c0880e82bf0db8efcda4c9b1695deb8e8fb
Miunn <remcaulier@orange.fr>
2024-01-02 18:07:20 +0100

    Navigation on bottle summary

--- src/components/BottleSummary.tsx
+++ src/components/BottleSummary.tsx
@@ -3,23 +3,25 @@ import {useTheme} from "@react-navigation/native";
 import {Button, Card, Icon, TouchableRipple} from "react-native-paper";
 import {deleteBottle, getDBConnection} from "../services/db-interface.ts";
 import {BottleType} from "../models/BottleType.tsx";
+import {PressableOpacity} from "react-native-pressable-opacity";
 
 interface BottleSummaryProps {
     bottle: BottleType,
+    navigation: any
 }
 
-export const BottleSummary = ({bottle: {id, name, vintageYear, color}}: BottleSummaryProps) => {
+export const BottleSummary = ({bottle, navigation}: BottleSummaryProps) => {
 
     const {colors} = useTheme();
     const icon = (props: { size: number }) => <Icon source={"bottle-wine"} size={20}/>
 
     const deleteBottleFromDB = async () => {
         const db = await getDBConnection();
-        await deleteBottle(db, id!);
+        await deleteBottle(db, bottle.id!);
     };
 
     let colorString;
-    switch (color) {
+    switch (bottle.color) {
         case 'red':
             colorString = "Vin Rouge";
             break;
@@ -30,14 +32,16 @@ export const BottleSummary = ({bottle: {id, name, vintageYear, color}}: BottleSu
             colorString = "Vin Rouge";
             break;
         default:
-            colorString = `Vin ${color}`;
+            colorString = `Vin ${bottle.color}`;
     }
 
     return (
-        <Card style={{
-            margin: 10
-        }}>
-            <Card.Title title={name} subtitle={`${vintageYear} - ${colorString}`} left={icon}/>
+        <Card
+            style= {{
+                    margin: 10
+                }}
+            onPress={() => navigation.navigate("bottle", {bottle: bottle})}>
+            <Card.Title title={bottle.name} subtitle={`${bottle.vintageYear} - ${colorString}`} left={icon}/>
             <Card.Content>
                 <Text style={{
                     color: colors.text
@@ -46,6 +50,7 @@ export const BottleSummary = ({bottle: {id, name, vintageYear, color}}: BottleSu
             <Card.Actions>
                 <Button mode={"text"} onPress={deleteBottleFromDB}>Sortir la bouteille</Button>
             </Card.Actions>
+
         </Card>
     )
 }
