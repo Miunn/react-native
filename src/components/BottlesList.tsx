b2e3322e0198f8d5dd0662091f7bd940bfeb045f
Miunn <remcaulier@orange.fr>
2024-01-07 22:20:35 +0100

    Bottle list accurate colors

--- src/components/BottlesList.tsx
+++ src/components/BottlesList.tsx
@@ -31,13 +31,13 @@ const BottlesList = ({navigation}: any) => {
                 let colorString;
                 switch (bottle.color) {
                     case 'red':
-                        colorString = "Vin Rouge";
+                        colorString = "Rouge";
                         break;
                     case 'white':
-                        colorString = "Vin Rouge";
+                        colorString = "Blanc";
                         break;
                     case 'pink':
-                        colorString = "Vin Rouge";
+                        colorString = "Rosé";
                         break;
                     default:
                         colorString = `Vin ${bottle.color}`;
